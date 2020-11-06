cb98be841f0f1e66d112b5d8663c6e6174437d78
Rogerio Colaço <rcs@ufscar.br>
2020-11-06 19:52:39 -0300

    - gitignore

--- rotas.js
+++ rotas.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+/*ROTAS TELA DE LOGIN*/
+
 app.get("/", (req, res) => {
     res.send("rota para a página de login");
 });
@@ -16,6 +18,21 @@ app.post("/minhaAgenda", (req, res) => {
 app.get("/logout", (req, res) => {
     res.send("rota para que usuário logado possa sair do sistema");
 });
+
+/*ROTAS NAVEGAÇÃO ENTRE AGENDA DE CONTATOS E COMPROMISSO*/
+
+app.get("/meusCotatos", (req, res) => {
+    res.send("rota utilizada caso o usuário opte por acessar sua agenda de contatos");
+});
+
+app.get("/meusCompromissos", (req, res) => {
+    res.send("rota utilizada caso o usuário opte por acessar sua agenda de compromissos");
+});
+
+/*ROTAS CRUD COMPROMISSOS*/
+
+
+/*ROTAS CRUD CONTATOS DE AGENDA*/
 
 app.listen(8080, function () {
     console.log("Rodando na porta 8080");
