ae6b3b752a73b44f03cb4ba5743ec25a589e3f8d
Roger <roger.peratello@hotmail.com>
2020-11-26 00:27:44 -0300

    finalizada conexão com banco

--- conexao.js
+++ conexao.js
@@ -11,6 +11,7 @@ async function conecta() {
     return con;
 }
 
+//usuarios
 async function listaTodosUsuarios() {
     console.log("Listando todos usuários");
     const conexaoAtiva = await conecta();
@@ -47,4 +48,82 @@ async function alteraUsuario(usuario) {
     return await conexaoAtiva.query(sql, parametros);
 }
 
-module.exports = { listaTodosUsuarios, selecionaUsuario, insereUsuario, excluiUsuario, alteraUsuario }
+//contato
+async function listaTodosContatos() {
+    console.log("Listando todos os contatos");
+    const conexaoAtiva = await conecta();
+    const [resultado] = await conexaoAtiva.query("SELECT * FROM contatos");
+    return resultado;
+}
+
+async function selecionaContato(id) {
+    console.log(`Selecionado o contato com id: ${id}`);
+    const conexaoAtiva = await conecta();
+    const [resultado] = await conexaoAtiva.query("SELECT * FROM contatos WHERE id=?;", [id]);
+    return resultado;
+}
+
+async function insereContato(contato) {
+    console.log("Inserindo contato " +  contato.nome);
+    const conexaoAtiva = await conecta();
+    const sql = "INSERT INTO contato(nome, email, telefone, endereco, user_id) VALUES (?,?,?,?,?);";
+    const parametros = [contato.nome, contato.email, contato.telefone, contato.endereco, contato.user_id];
+    return await conexaoAtiva.query(sql, parametros);
+}
+
+async function excluiContato(id) {
+    console.log(`Apagando o contato com id:${id}`);
+    const conexaoAtiva = await conecta();
+    return await conexaoAtiva.query("DELETE FROM contato WHERE id=?", [id]);
+}
+
+async function alteraContato(usuario) {
+    console.log("Alterando usuário: " +  usuario.nome);
+    const conexaoAtiva = await conecta();
+    const sql = "UPDATE usuario SET nome = ?, email = ?, telefone = ?, endereco = ?, user_id = ? WHERE id = ?;";
+    const parametros = [contato.nome, contato.email, contato.telefone, contato.endereco, contato.user_id, contato.id];
+    return await conexaoAtiva.query(sql, parametros);
+}
+
+//compromisso
+async function listaTodosCompromissos() {
+    console.log("Listando todos os compromissos");
+    const conexaoAtiva = await conecta();
+    const [resultado] = await conexaoAtiva.query("SELECT * FROM compromisso");
+    return resultado;
+}
+
+async function selecionaCompromisso(id) {
+    console.log(`Selecionado o compromisso com id: ${id}`);
+    const conexaoAtiva = await conecta();
+    const [resultado] = await conexaoAtiva.query("SELECT * FROM compromisso WHERE id=?;", [id]);
+    return resultado;
+}
+
+async function insereCompromisso(compromisso) {
+    console.log("Inserindo compromisso: " +  compromisso.nome);
+    const conexaoAtiva = await conecta();
+    const sql = "INSERT INTO compromisso(data, obs, participantes, endereco, status, user_id) VALUES (?,?,?,?,?,?);";
+    const parametros = [compromisso.data, compromisso.obs, compromisso.participantes, compromisso.endereco, compromisso.status, compromisso.user_id];
+    return await conexaoAtiva.query(sql, parametros);
+}
+
+async function excluiCompromisso(id) {
+    console.log(`Apagando o compromisso com id:${id}`);
+    const conexaoAtiva = await conecta();
+    return await conexaoAtiva.query("DELETE FROM compromisso WHERE id=?", [id]);
+}
+
+async function alteraCompromisso(compromisso) {
+    console.log("Alterando compromisso: " +  compromisso.id);
+    const conexaoAtiva = await conecta();
+    const sql = "UPDATE usuario SET data = ?, obs = ?, participantes = ?, endereco = ?,  status = ?, user_id = ? WHERE id = ?;";
+    const parametros = [compromisso.data, compromisso.obs, compromisso.participantes, compromisso.endereco, compromisso.status, compromisso.user_id, compromisso.id];
+    return await conexaoAtiva.query(sql, parametros);
+}
+
+module.exports = {
+    listaTodosUsuarios, selecionaUsuario, insereUsuario, excluiUsuario, alteraUsuario,
+    listaTodosContatos, selecionaContato, insereContato, excluiContato, alteraContato,
+    listaTodosCompromissos, selecionaCompromisso, insereCompromisso, excluiCompromisso, alteraCompromisso,
+}
